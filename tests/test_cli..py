0dd017d8552b5f46a88ea7e9f269db00ca7be07d
Ashton Hellwig <ahellwig.dev@gmail.com>
2020-05-14 00:54:50 -0600

    add placeholder tests

---
+++ tests/test_cli..py
@@ -0,0 +1,4 @@
+class TestCLI:
+    def test_cli_1(self) -> None:
+        x: str = "placeholder"
+        assert "h" in x
---
+++ tests/test_version.py
@@ -0,0 +1,12 @@
+class TestVersion:
+    def test_version_file(self) -> None:
+        x: str = "placeholder"
+        assert "h" in x
+
+    def test_setup_file(self) -> None:
+        x: str = "placeholder"
+        assert "h" in x
+
+    def test_meta_file(self) -> None:
+        x: str = "placeholder"
+        assert "h" in x
